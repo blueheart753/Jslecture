2c28d89fe21f2e4823b028681271937c84d0e04e
kimminam <bluemind8520@gmail.com>
2023-02-21 10:22:56 +0900

    [feat]  Sting

---
+++ day2/app.js
@@ -0,0 +1,39 @@
+const str1 = "hello world";
+// str1 = new String("hi");
+
+console.log(str1.charAt(1));
+console.log(str1[1]);
+
+console.log(str1.charAt(1));
+console.log(str1[1]);
+
+console.log(str1.indexOf("world"));
+console.log(str1.indexOf("@orld"));
+
+console.log(str1.lastIndexOf("world"));
+
+console.log(str1.match("@worid"));
+
+console.log(str1.replace("world","JavaScript"));
+
+console.log(str1.search("world"));
+
+
+const str = "The quick brown fox jusmps over the lazzy dog.";
+
+console.log(str.slice(31));
+
+console.log(str.slice(4,19));
+
+console.log(str.slice(-4));
+
+console.log(str.slice(-9,-5));
+
+console.log(str.substring[4,9]);
+
+console.log(str1.toUpperCase());
+
+console.log(str1.toLowerCase());
+
+const str2 = "                            hello                     "
+console.log(str2.trim());
