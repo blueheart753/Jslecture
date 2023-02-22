61a368db47cb93a72cc1ff35d2f48162e6ee1a3a
kimminam <bluemind8520@gmail.com>
2023-02-22 11:33:59 +0900

    탐색선택자 내용 예제

---
+++ day3/selector2.js
@@ -0,0 +1,17 @@
+// $("#menu li:first").css("background", "#f70");
+// $("#menu li:last").css("background", "#f7f");
+
+// $("#menu li:even").css("color", "red");
+// $("#menu li:odd").css("color", "blue");
+
+// $("#menu li:nth-child(2n)").css("color", "black");
+// $("#menu li:nth-child(2n+1)").css("color", "blue");
+
+// $("#menu li:nth-last-child(2)").css("color", "blue");
+
+// $("#menu li:first-of-type").css("color", "salmon");
+// $("#menu li:last-of-type").css("color", "skyblue");
+
+// $("li:only-child").css("border", "1px solid salmon");
+
+// $("#menu li").slice(1, 3).css("background", "salmon");
